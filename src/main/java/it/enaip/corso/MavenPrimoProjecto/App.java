99b1d6eeca5f2a90d2e6ee70cb745968e4000d3a
ottoemilio <ottoemilio@gmail.com>
2020-06-25 13:15:53 +0200

    Ce ancora tanto per fare

---
+++ src/main/java/it/enaip/corso/MavenPrimoProjecto/App.java
@@ -0,0 +1,13 @@
+package it.enaip.corso.MavenPrimoProjecto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
